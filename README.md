d2035a42037cc576af71b60703b3ff3c506b8022
flaaim <flaaim@list.ru>
2023-02-20 19:35:02 +1000

    php skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+# PHP Skeleton
+
+Простой скелет для php проектов.
+
+## Установка
+
+1. Composer
+```
+    composer require flaaim/php-skeleton
+```
---
+++ app/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Hello 
+{
+    public static function greeting()
+    {
+        return print_r('Hello world');
+    }
+}
